df8f3d3543cca2cd6d00edcd9dc11acda752daef
Haneol <haneolj@gmail.com>
2025-02-10 09:45:35 +0900

    Initial Commit

---
+++ src/main/java/com/myme/mywarehome/MywarehomeApplication.java
@@ -0,0 +1,13 @@
+package com.myme.mywarehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywarehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywarehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myme/mywarehome/MywarehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myme.mywarehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywarehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
