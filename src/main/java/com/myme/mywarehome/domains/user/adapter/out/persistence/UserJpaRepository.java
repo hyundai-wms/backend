61ba211f031e0f360dc8f9309fc822c56ef3d2c5
Haneol <haneolj@gmail.com>
2025-02-11 16:40:47 +0900

    [feature/#1] feat : @Repository 추가

--- src/main/java/com/myme/mywarehome/domains/user/adapter/out/persistence/UserJpaRepository.java
+++ src/main/java/com/myme/mywarehome/domains/user/adapter/out/persistence/UserJpaRepository.java
@@ -2,9 +2,11 @@ package com.myme.mywarehome.domains.user.adapter.out.persistence;
 
 import com.myme.mywarehome.domains.user.application.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(Long userId);
     Optional<User> findById(String id);
