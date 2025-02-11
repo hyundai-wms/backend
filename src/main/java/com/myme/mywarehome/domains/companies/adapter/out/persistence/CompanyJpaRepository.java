c9aa273342bef7e9bf7f55880d1c9bf3ba472179
zlnongi <yeossi8@naver.com>
2025-02-11 17:11:40 +0900

    [feature/#9] fix: 납품업체 필터링 기능 추가 재수정

--- src/main/java/com/myme/mywarehome/domains/companies/adapter/out/persistence/CompanyJpaRepository.java
+++ src/main/java/com/myme/mywarehome/domains/companies/adapter/out/persistence/CompanyJpaRepository.java
@@ -7,15 +7,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyJpaRepository extends JpaRepository<Company, Long> {
     // Todo: mybatis로 변경
     @Query("SELECT c FROM Company c WHERE c.isVendor = true " +
-            "AND c.companyCode LIKE :code " +
-            "AND c.companyName LIKE :name")
+            "AND (c.companyCode LIKE CONCAT('%' , COALESCE(:code, ''), '%')" +
+            "OR c.companyName LIKE CONCAT('%' , COALESCE(:name, ''), '%'))")
     Page<Company> findVendorsByConditions(
             @Param("code") String code,
             @Param("name") String name,
             Pageable pageable
     );
 }
+
+
