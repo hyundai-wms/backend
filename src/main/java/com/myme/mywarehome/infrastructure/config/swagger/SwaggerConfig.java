5cec0d59088dcb0269fb9575d89545d9776c556f
Haneol <haneolj@gmail.com>
2025-02-21 20:58:54 +0900

    [deploy/#142] chore: swagger config 일부 수정

--- src/main/java/com/myme/mywarehome/infrastructure/config/swagger/SwaggerConfig.java
+++ src/main/java/com/myme/mywarehome/infrastructure/config/swagger/SwaggerConfig.java
@@ -2,8 +2,6 @@ package com.myme.mywarehome.infrastructure.config.swagger;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.tags.Tag;
 import java.util.Arrays;
@@ -26,15 +24,6 @@ public class SwaggerConfig {
                 .version("v1.0")
                 .description("MyWareHome API 문서입니다.");
 
-        // Security 스키마 설정
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        // Security 요청 설정
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
         // Tags 순서 설정
         List<Tag> tags = Arrays.asList(
                 new Tag().name("Auth").description("인증 관련 API")
@@ -43,7 +32,6 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .addServersItem(new Server().url(appBaseUrl).description("Production"))
                 .info(info)
-                .addSecurityItem(securityRequirement)
                 .tags(tags);
     }
 
